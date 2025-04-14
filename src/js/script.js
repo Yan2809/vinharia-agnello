53eb3d7deccc98eb2951c12f3d82532a3117897b
guilherme de araujo <guiam2909@gmail.com>
2025-04-14 14:30:06 -0300

    Solicitando informacoes do vinho

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+// 1- Solicitando informações pelo prompt
+let nomeVinho = prompt("Digite o nome do vinho a ser cadastrado:")
+let tipoVinho = prompt("Qual o tipo do vinho?")
+let safraVinho = prompt("De que ano ele é?")
+let quantEstoque = prompt("Quantos vinhos há em estoque?")
+
+// 3- Exibindo alert de cadastro realizado
+alert("Cadastro realizado! Veja os detalhes no console.")
+
+console.log("===========================================")
+console.log("============= Vinho Cadastrado ============")
+console.log(`Nome do vinho: ${nomeVinho}`)
+console.log(`Tipo do vinho: ${tipoVinho}`)
+console.log(`Ano da safra: ${safraVinho}`)
+console.log(`Quantidade em estoque: ${quantEstoque}`)
+
